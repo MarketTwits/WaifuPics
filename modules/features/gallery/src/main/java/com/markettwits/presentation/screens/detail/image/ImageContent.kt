89412d1aeaf8c9f0c055000b3d45ca230601a679
daniil <daniil-kulakov@list.ru>
2023-11-27 14:39:45 +0700

    fix double click

--- modules/features/gallery/src/main/java/com/markettwits/presentation/screens/detail/image/ImageContent.kt
+++ modules/features/gallery/src/main/java/com/markettwits/presentation/screens/detail/image/ImageContent.kt
@@ -72,6 +72,7 @@ fun ZoomablePagerImage(
                 .combinedClickable(
                     interactionSource = remember { MutableInteractionSource() },
                     indication = null,
+                    onDoubleClick = {},
                     onClick = onItemClick
                 )
                 .zoomable(
